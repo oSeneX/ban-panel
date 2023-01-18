fdadefa654f6464df61f3ada97fb5e7442e743af
SeneX <46788984+oSeneX@users.noreply.github.com>
2023-01-18 23:06:53 +0200

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'SeneX'
+description ''
+version '1.0.0'
+
+server_scripts {
+    'src/sv_main.js',
+}
---
+++ src/sv_main.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const url = require("url");
+
+const htmlFile = LoadResourceFile(GetCurrentResourceName(), 'web/index.html')
+const cssFile = LoadResourceFile(GetCurrentResourceName(), 'web/style.css')
+const configFile = LoadResourceFile(GetCurrentResourceName(), 'config.json')
+
+const configObj = JSON.parse(configFile)
+const apikey = configObj.apikey
+if (apikey == 'apikey' || apikey == '') { console.log('API-avainta ei ole asetettu.'); }
+
+http.createServer(function (req, res) {
+    const reqUrl = url.parse(req.url).pathname
+    if(reqUrl == "/") {
+        res.setHeader("Content-Type", "text/html");
+        res.end(htmlFile);
+    } else if (reqUrl === '/web/style.css') {
+        res.end(cssFile);
+    }
+
+}).listen(50120);
+
