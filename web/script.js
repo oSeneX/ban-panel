5b94ec76502d0e9d5d0f8bdaad5df485b7ddf83a
SeneX <46788984+oSeneX@users.noreply.github.com>
2023-01-19 22:49:32 +0200

    feat(ui): ban search

--- web/script.js
+++ web/script.js
@@ -1,7 +1,7 @@
 var currentPage = '#manage-bans-block';
 
-const addPlayerToTable = function(servername, playername, reason, time, steam, discord, rockstar, live, xbox) {
-    $('#player-list').append(`
+const addPlayerToList = function(servername, playername, reason, time, steam, discord, rockstar, live, xbox) {
+    $('#player-list tbody').append(`
     <tr>
         <td><label class="switch"><input type="checkbox"><span class="slider round"></span></label></td>
         <td>`+servername+`</td>
@@ -16,19 +16,39 @@ const addPlayerToTable = function(servername, playername, reason, time, steam, d
     </tr>`);
 }
 
+const activeTab = function(value) {
+    var href = $(value).attr('href');
+    $(currentPage).css('display', 'none');
+    $(href).css('display', 'flex');
+    $('li').removeClass();
+    $(value).parent().addClass('active');
+    currentPage = href;
+}
+
 $(function() {
-    $(currentPage).css('display', 'flex');
+    $(currentPage).css('display', 'flex'); //Show default tab
     $('li > a').click(function() {
-        var href = $(this).attr('href');
-        $(currentPage).css('display', 'none');
-        $(href).css('display', 'flex');
-        $('li').removeClass();
-        $(this).parent().addClass('active');
-        currentPage = href;
+        activeTab(this) //Switch to other tab
     });
 
-    $('#player-list').empty().append(`<tr><th>Käytä</th><th>Palvelin</th><th>Pelaaja</th><th>Syy</th><th>Kesto</th><th>Steam</th><th>Discord</th><th>Rockstar</th><th>Live</th><th>Xbox</th></tr>`);
-    /* Example player
-    addPlayerToTable('Example Roleplay', 'Pelaaja 1', 'Koodari', 1234, 'steam:4343', 'discord:4343', 'rockstar:4343', 'live:4343', 'xbox:4343');*/
+    $('#player-list').empty().append(`<thead><tr><th>Käytä</th><th>Palvelin</th><th>Pelaaja</th><th>Syy</th><th>Kesto</th><th>Steam</th><th>Discord</th><th>Rockstar</th><th>Live</th><th>Xbox</th></tr></thead><tbody></tbody>`);
+    
+    $('#player-list-search').keyup(function(){ //Search
+        var search = $(this).val();
+        $('table tbody tr').hide();
+    
+        var len = $('table tbody tr:not(.notfound) td:contains("'+search+'")').length;
+    
+        if (len > 0) {
+            $('table tr:not(.notfound) td:contains("'+search+'")').each(function(){
+                $(this).closest('tr').show();
+            });
+        }
+    });
+    /* Example bans*/
+    addPlayerToList('Example Roleplay', 'Pelaaja 1', 'Koodari', 1234, 'steam:4343', 'discord:4343', 'rockstar:4343', 'live:4343', 'xbox:4343');
+    addPlayerToList('Example Roleplay', 'Pelaaja 2', 'Koodari', 654, 'steam:4343', 'discord:4343', 'rockstar:4343', 'live:4343', 'xbox:4343');
+    addPlayerToList('Example Roleplay', 'Pelaaja 3', 'Koodari', 54, 'steam:4343', 'discord:4343', 'rockstar:4343', 'live:4343', 'xbox:4343');
+    addPlayerToList('Example Roleplay', 'Pelaaja 4', 'Koodari', 745, 'steam:4343', 'discord:4343', 'rockstar:4343', 'live:4343', 'xbox:4343');
 });
 
